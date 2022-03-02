ed354277df6f2251993e4a9273de31c0dc644b36
Jesse-Snow <jesse.neves@outlook.com>
2022-03-02 12:27:08 -0300

    Base server working

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req,res)=>{
+  // Rota da requisição
+  if( req.url === '/api' && req.method === 'GET'){
+    // Response Header
+    res.writeHead(200,{"Content-Type":"text/html"});
+    // Response 
+    res.write("<h1>Oi, o servidor funcionou</h1>");
+    // Response end
+    res.end();
+  }
+  // Caso não tenha rota
+  else{
+    res.writeHead(404,{"Content-Type":"application/json"});
+    res.end(JSON.stringify({message:"Rota não encontrada"}))
+  }
+})
+
+server.listen(port,()=> console.log(`Server running on Port:${port}`));
